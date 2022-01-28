1ea37caee4f2ad18b90c7ad7097d28ab8df536c7
wyt2019suzhou <13508894496@163.com>
2022-01-28 14:59:34 +0800

    code for binarized FCMNet in pathfinding task

--- environments/__init__.py
+++ environments/__init__.py
@@ -2,6 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='MultiagentCatch-v0',
-    entry_point='environments.scenarios:MultiAgentEnv',
-    max_episode_steps=1024,
+    entry_point='environments.scenarios:MultiAgentEnv'
 )
